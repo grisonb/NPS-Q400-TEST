1b741a79b2b9f9c41b93d96bc3a13e2f50f18ab6
grisonb <79510016+grisonb@users.noreply.github.com>
2025-08-11 08:11:04 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 // --- FICHIER sw.js SANS TÉLÉCHARGEMENT DE MASSE ---
 
-const APP_CACHE_NAME = 'communes-app-cache-v117'; // Version 10.0
-const DATA_CACHE_NAME = 'communes-data-cache-v117';
-const TILE_CACHE_NAME = 'communes-tile-cache-v117';
+const APP_CACHE_NAME = 'communes-app-cache-v118'; // Version 10.0
+const DATA_CACHE_NAME = 'communes-data-cache-v118';
+const TILE_CACHE_NAME = 'communes-tile-cache-v118';
 
 const APP_SHELL_URLS = [
     './',
