502993b4c1538720e14509307f510f376801895d
grisonb <79510016+grisonb@users.noreply.github.com>
2025-08-12 14:13:26 +0200

    v51.0.js

--- script.js
+++ script.js
@@ -118,7 +118,7 @@ function setupEventListeners() {
     if (mainActionButtons) {
         const versionDisplay = document.createElement('div');
         versionDisplay.className = 'version-display';
-        versionDisplay.innerText = 'v50.9';
+        versionDisplay.innerText = 'v51.0';
         mainActionButtons.appendChild(versionDisplay);
     }
 
