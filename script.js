da83cfe69f6592dc7d5cb434696ae869ee6df655
grisonb <79510016+grisonb@users.noreply.github.com>
2026-01-09 10:55:43 +0100

    Update version display to v5.1

--- script.js
+++ script.js
@@ -163,7 +163,7 @@ function setupEventListeners() {
     if (mainActionButtons) {
         const versionDisplay = document.createElement('div');
         versionDisplay.className = 'version-display';
-        versionDisplay.innerText = 'v62.0';
+        versionDisplay.innerText = 'v5.1';
         mainActionButtons.appendChild(versionDisplay);
     }
 
