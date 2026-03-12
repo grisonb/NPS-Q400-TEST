489757963b5952f546322fa58174b995d94f9fb4
grisonb <79510016+grisonb@users.noreply.github.com>
2026-03-12 12:57:27 +0100

    Improve clear-search touch target and bump app to v6.2

--- script.js
+++ script.js
@@ -163,7 +163,7 @@ function setupEventListeners() {
     if (mainActionButtons) {
         const versionDisplay = document.createElement('div');
         versionDisplay.className = 'version-display';
-        versionDisplay.innerText = 'v6.1';
+        versionDisplay.innerText = 'v6.2';
         mainActionButtons.appendChild(versionDisplay);
     }
 
