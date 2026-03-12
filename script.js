ee7f0aca38220b28fd2157b6ac060ca83bd56910
grisonb <79510016+grisonb@users.noreply.github.com>
2026-03-12 13:21:01 +0100

    Align versioning to v6.2/v602 to avoid merge conflicts

--- script.js
+++ script.js
@@ -163,7 +163,7 @@ function setupEventListeners() {
     if (mainActionButtons) {
         const versionDisplay = document.createElement('div');
         versionDisplay.className = 'version-display';
-        versionDisplay.innerText = 'v6.3';
+        versionDisplay.innerText = 'v6.2';
         mainActionButtons.appendChild(versionDisplay);
     }
 
