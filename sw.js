f4ee445071c99cad30e6ef1f99d7b3990df2e57d
grisonb <79510016+grisonb@users.noreply.github.com>
2026-03-12 18:51:12 +0100

    Update cache names to version v710

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const APP_CACHE_NAME = 'test-communes-app-cache-v700'; 
-const DATA_CACHE_NAME = 'test-communes-data-cache-v700';
-const TILE_CACHE_NAME = 'test-communes-tile-cache-v700';
+const APP_CACHE_NAME = 'test-communes-app-cache-v710'; 
+const DATA_CACHE_NAME = 'test-communes-data-cache-v710';
+const TILE_CACHE_NAME = 'test-communes-tile-cache-v710';
 
 const APP_SHELL_URLS = [
     './',
