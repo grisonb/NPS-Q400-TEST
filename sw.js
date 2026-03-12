805992443b694a50a6e6a556664981d0a8efc4a4
grisonb <79510016+grisonb@users.noreply.github.com>
2026-03-12 17:42:13 +0100

    Add selectable BASE airport for routing and calculations (v7.0/v700)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const APP_CACHE_NAME = 'test-communes-app-cache-v605'; 
-const DATA_CACHE_NAME = 'test-communes-data-cache-v605';
-const TILE_CACHE_NAME = 'test-communes-tile-cache-v605';
+const APP_CACHE_NAME = 'test-communes-app-cache-v700'; 
+const DATA_CACHE_NAME = 'test-communes-data-cache-v700';
+const TILE_CACHE_NAME = 'test-communes-tile-cache-v700';
 
 const APP_SHELL_URLS = [
     './',
