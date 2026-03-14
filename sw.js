20ed84853ab45d1e58f4d8d31151950fadc29916
grisonb <79510016+grisonb@users.noreply.github.com>
2026-03-14 12:44:19 +0100

    Update cache names to version v801

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const APP_CACHE_NAME = 'test-communes-app-cache-v800'; 
-const DATA_CACHE_NAME = 'test-communes-data-cache-v800';
-const TILE_CACHE_NAME = 'test-communes-tile-cache-v800';
+const APP_CACHE_NAME = 'test-communes-app-cache-v801'; 
+const DATA_CACHE_NAME = 'test-communes-data-cache-v801';
+const TILE_CACHE_NAME = 'test-communes-tile-cache-v801';
 
 const APP_SHELL_URLS = [
     './',
