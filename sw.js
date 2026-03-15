e16f500f8be8042cdf65a47be4082f06ba1788d1
grisonb <79510016+grisonb@users.noreply.github.com>
2026-03-15 15:59:45 +0100

    Force service worker refresh and bump app version to v8.14

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const APP_CACHE_NAME = 'test-communes-app-cache-v802'; 
-const DATA_CACHE_NAME = 'test-communes-data-cache-v802';
-const TILE_CACHE_NAME = 'test-communes-tile-cache-v802';
+const APP_CACHE_NAME = 'test-communes-app-cache-v814'; 
+const DATA_CACHE_NAME = 'test-communes-data-cache-v814';
+const TILE_CACHE_NAME = 'test-communes-tile-cache-v814';
 
 const APP_SHELL_URLS = [
     './',
