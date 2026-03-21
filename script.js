f9728082361f28e0683c750df4bc2b63b82a4fe2
grisonb <79510016+grisonb@users.noreply.github.com>
2026-03-21 13:06:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -253,7 +253,7 @@ function setupEventListeners() {
     if (mainActionButtons) {
         const versionDisplay = document.createElement('div');
         versionDisplay.className = 'version-display';
-        versionDisplay.innerText = 'v8.24';
+        versionDisplay.innerText = 'v8.26';
         mainActionButtons.appendChild(versionDisplay);
     }
 
