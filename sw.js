f428a5dbcef758921cd3e7b7d4a7904343560c20
grisonb <79510016+grisonb@users.noreply.github.com>
2026-03-21 17:05:20 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const APP_CACHE_NAME = 'test-communes-app-cache-v833'; 
-const DATA_CACHE_NAME = 'test-communes-data-cache-v833';
-const TILE_CACHE_NAME = 'test-communes-tile-cache-v833';
+const APP_CACHE_NAME = 'test-communes-app-cache-v834'; 
+const DATA_CACHE_NAME = 'test-communes-data-cache-v834';
+const TILE_CACHE_NAME = 'test-communes-tile-cache-v834';
 
 const APP_SHELL_URLS = [
     './',
