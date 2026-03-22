be8f3e349da4abf5a862250d794130f3c66744aa
grisonb <79510016+grisonb@users.noreply.github.com>
2026-03-22 21:19:26 +0100

    Restaure purge historique canal à 12h et passe en v8.53

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const APP_CACHE_NAME = 'test-communes-app-cache-v844'; 
-const DATA_CACHE_NAME = 'test-communes-data-cache-v844';
-const TILE_CACHE_NAME = 'test-communes-tile-cache-v844';
+const APP_CACHE_NAME = 'test-communes-app-cache-v853'; 
+const DATA_CACHE_NAME = 'test-communes-data-cache-v853';
+const TILE_CACHE_NAME = 'test-communes-tile-cache-v853';
 
 const APP_SHELL_URLS = [
     './',
