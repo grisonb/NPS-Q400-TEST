713219b5c68debf6c36235fb94b31b833d3872e3
grisonb <79510016+grisonb@users.noreply.github.com>
2026-03-23 15:51:33 +0100

    Inverse positions commune proche et zoom, passe en v8.57

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const APP_CACHE_NAME = 'test-communes-app-cache-v844'; 
-const DATA_CACHE_NAME = 'test-communes-data-cache-v844';
-const TILE_CACHE_NAME = 'test-communes-tile-cache-v844';
+const APP_CACHE_NAME = 'test-communes-app-cache-v857'; 
+const DATA_CACHE_NAME = 'test-communes-data-cache-v857';
+const TILE_CACHE_NAME = 'test-communes-tile-cache-v857';
 
 const APP_SHELL_URLS = [
     './',
