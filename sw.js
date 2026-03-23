0be3f108d839603db5de16d0f7b51bf1426059e4
grisonb <79510016+grisonb@users.noreply.github.com>
2026-03-23 16:09:12 +0100

    Corrige l'ordre des messages après reconnexion et passe en v8.58

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const APP_CACHE_NAME = 'test-communes-app-cache-v844'; 
-const DATA_CACHE_NAME = 'test-communes-data-cache-v844';
-const TILE_CACHE_NAME = 'test-communes-tile-cache-v844';
+const APP_CACHE_NAME = 'test-communes-app-cache-v858'; 
+const DATA_CACHE_NAME = 'test-communes-data-cache-v858';
+const TILE_CACHE_NAME = 'test-communes-tile-cache-v858';
 
 const APP_SHELL_URLS = [
     './',
