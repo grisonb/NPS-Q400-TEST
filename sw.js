2c7e35f4bfc70ee958dc0ee15a26b51e9d1d0acc
grisonb <79510016+grisonb@users.noreply.github.com>
2026-04-12 08:44:53 +0200

    Fix iPad loading error and bump app cache version to v8.89

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const APP_CACHE_NAME = 'test-communes-app-cache-v888'; 
-const DATA_CACHE_NAME = 'test-communes-data-cache-v888';
-const TILE_CACHE_NAME = 'test-communes-tile-cache-v888';
+const APP_CACHE_NAME = 'test-communes-app-cache-v889'; 
+const DATA_CACHE_NAME = 'test-communes-data-cache-v889';
+const TILE_CACHE_NAME = 'test-communes-tile-cache-v889';
 
 const APP_SHELL_URLS = [
     './',
